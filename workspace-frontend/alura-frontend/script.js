833290486e6bb62ab87845fd379c2fcb47e70cd4
JoseJacsan <jjacsan@gmail.com>
2022-03-20 12:24:55 -0300

    Projetos atualizados

---
+++ workspace-frontend/alura-frontend/script.js
@@ -0,0 +1,13 @@
+var n = 24;
+
+if (n % 2 == 0 && n > 24) {
+    print("Alohomora");
+}
+
+if (n * 2 < 50 || n % 2 != 0) {
+    print("Expelliarmus");
+}
+
+if ((n + 1 == 25 && n / 6 == 4) || n < 24) {
+    print("Expecto")
+}
